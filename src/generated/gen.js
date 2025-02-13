21827e9bd2bad4d191288cf1a6d88022d020dd85
Marcel Wiessler <marcel@gaisterhand.de>
2025-02-13 16:49:35 +0100

    Update 3d scene

--- src/generated/gen.js
+++ src/generated/gen.js
@@ -2,13 +2,13 @@
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("assets/Particle%20Custom%20Behaviour.glb?v=1739461641189");
+needle_exported_files.push("assets/Particle%20Custom%20Behaviour.glb?v=1739461759303");
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1739461641189");
+		needleEngine.setAttribute("hash", "1739461759303");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
