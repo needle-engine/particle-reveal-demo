182e5606e6188a1d78f9afd4d46b7cd0588c42f6
Marcel Wiessler <marcel@gaisterhand.de>
2025-02-13 16:29:41 +0100

    add some generated folder

---
+++ src/generated/register_types.ts
@@ -0,0 +1,8 @@
+﻿/* eslint-disable */
+import { TypeStore } from "@needle-tools/engine"
+
+// Import types
+import { CustomParticleBehaviour_Reveal } from "../scripts/CustomParticleBehaviour.js";
+
+// Register types
+TypeStore.add("CustomParticleBehaviour_Reveal", CustomParticleBehaviour_Reveal);
